9591847892622157bd55f6a1e11b2458d0878f94
fatimasajadi <s.sfatemeh@gmail.com>
2020-06-02 18:40:20 +0000

    experimenting with sending commands to the server

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ const net = require('net');
 // let port = 50541;
 // let host = '10.0.2.15';
 // Establishes connection with the game server
+
 const connect = function() {
   const conn = net.createConnection({
     host: '10.0.2.15',
@@ -10,13 +11,35 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
-  conn.on('connect', () => {
-    console.log("Successfully connected to game server");
-  });
 
   conn.on('connect', () => {
-    conn.write('Name: FSS');
+    console.log("Successfully connected to game server");
+
+    conn.write('Name: FSS', () => {
+      // setTimeout(() => {
+      //   conn.write('Move: up', () => {
+      //     setTimeout(() => {
+      //       conn.write('Move: left')
+
+      //     }, 400);
+
+      //   });
+      // }, 400)
+    });
+
+
   });
+  // conn.on('connect', () => {
+  //   conn.write('Name: FSS');
+
+  // });
+
+  // conn.on('data', (data) => {
+
+  //   conn.write(data, 'Move: up');
+  // });
+
+
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
