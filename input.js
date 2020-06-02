725beb404be3c0cbfbdb3e52c20a61087311b673
fatimasajadi <s.sfatemeh@gmail.com>
2020-06-02 21:43:06 +0000

    Implement WASD Movement

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,9 +12,22 @@ const setupInput = function() {
 function handleUserInput(c) {
   const dataHex = Buffer.from(c, 'ascii').toString('hex');
   const ctrlCHex = '03';
+  const wKey = '57';
+  const aKey = '41';
+  const sKey = '53';
+  const dKey = '44';
 
   if (dataHex === ctrlCHex) {
     process.exit();
+  } else if (dataHex === sKey) {
+    connection.write('Move: down');
+  } else if (dataHex === aKey) {
+    connection.write('Move: left');
+  } else if (dataHex === dKey) {
+    connection.write('Move: right');
+  } else if (dataHex === wKey) {
+    connection.write('Move: up');
   }
 }
+
 module.exports = setupInput;
