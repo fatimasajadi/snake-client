165a29b7842db69ad31396f5be83fbd49d925a04
fatimasajadi <s.sfatemeh@gmail.com>
2020-06-02 16:14:22 +0000

    Sending snake name to the server

--- client.js
+++ client.js
@@ -10,9 +10,18 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: FSS');
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
   return conn;
 }
 module.exports = connect;
